2a394802dfac73d9514d24d3bcdaff7cb8aba340
salimkhan2927 <salimssm121@gmail.com>
2020-08-18 17:12:30 +0530

    Initial commit

---
+++ API/Controllers/ValuesController.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    
+   [Route("api/[controller]")]
+   [ApiController]
+    public class ValuesController:ControllerBase
+    {
+        private DataContext _repo;
+
+        public ValuesController(DataContext repo)
+        {
+            _repo=repo;
+        }
+
+          [HttpGet]
+        public async Task<ActionResult<IEnumerable<Value>>> Get()
+        {
+            var values=await _repo.Values.ToListAsync();
+            return Ok(values);
+        }
+
+       [HttpGet("{id}")]
+        public async Task<Value> Get(int id)
+        {
+            var value=await _repo.Values.FindAsync(id);
+            return value;
+        }
+
+        [HttpPost]
+        public void SaveNewValue([FromBody]string value)
+        {
+        }
+
+        [HttpPut]
+        public void UpdateValue(int id, [FromBody]string value)
+        {
+        }
+
+        [HttpDelete]
+        public void RemoveValue(int id)
+        {
+        }
+    }
+}
